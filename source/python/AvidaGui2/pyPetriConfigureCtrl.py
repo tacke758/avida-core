41f5c981538a2560ad909ea7b3391687d1c0d8a6
Brian Baer <baerb@msu.edu>
2006-06-14 13:15:33 +0000

    Made mutation slider 100 times more sensitive.
    Rearranged order of controls in Petri dish environmental control
    
    Added text edit boxes to allow exact entries for mutation rate and pop size

--- source/python/AvidaGui2/pyPetriConfigureCtrl.py
+++ source/python/AvidaGui2/pyPetriConfigureCtrl.py
@@ -38,8 +38,12 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     self.DishDisabled = False
     self.connect(self.MutationSlider, SIGNAL("valueChanged(int)"), 
       self.ChangeMutationTextSlot)
+    self.connect(self.MutationRateLineEdit, SIGNAL("returnPressed()"), 
+      self.ChangeMutationSliderSlot)
     self.connect(self.WorldSizeSlider, SIGNAL("valueChanged(int)"), 
       self.ChangeWorldSizeTextSlot)
+    self.connect(self.WorldSizeLineEdit, SIGNAL("returnPressed()"), 
+      self.ChangeWorldSizeSliderSlot)
     self.connect(self.StopManuallyRadioButton, SIGNAL("clicked()"), 
       self.ChangeStopSpinBoxEnabledSlot)
     self.connect(self.StopAtRadioButton, SIGNAL("clicked()"), 
@@ -118,25 +122,81 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     self.m_session_mdl = None
 
   
+  # When the user changes the mutation slider (which has a log scale) change the
+  # text next to it (which is liner)
+
   def ChangeMutationTextSlot(self):
-    slide_value = float(self.MutationSlider.value())/100.0
+    slide_value = float(self.MutationSlider.value())/10000.0
     slide_value = pow(10,slide_value)
     if slide_value < 0.0011:
       slide_value = 0.0
-    if slide_value > 1 or slide_value < 0.00001:
-      slide_value_txt = ("%1.1f" % (slide_value)) + "%"
+    if slide_value >= 1 or slide_value < 0.00001:
+      slide_value_txt = ("%1.1f" % (slide_value)) # + "%"
     elif slide_value > 0.1:
-      slide_value_txt = ("%1.2f" % (slide_value)) + "%"
+      slide_value_txt = ("%1.2f" % (slide_value)) # + "%"
     elif slide_value > 0.01:
-      slide_value_txt = ("%1.3f" % (slide_value)) + "%"
+      slide_value_txt = ("%1.3f" % (slide_value)) # + "%"
     else:
-      slide_value_txt = ("%1.4f" % (slide_value)) + "%"
-    self.MutationPercentTextLabel.setText(slide_value_txt)
+      slide_value_txt = ("%1.4f" % (slide_value)) # + "%"
+    self.MutationRateLineEdit.setText(slide_value_txt)
+  
+  # When the user changes the mutation slider (which has a log scale) change the
+  # text next to it (which is liner). Must check if the text entered is a 
+  # valid real number
+
+  def ChangeMutationSliderSlot(self):
+    validNumEntered = True
+    mutText = str(self.MutationRateLineEdit.text())
+    mutText = (mutText.rstrip('% ')).lstrip('+- ')
+    decPtLocation = mutText.find('.')
+    if (decPtLocation == -1):
+      if (not mutText.isdigit()):
+        validNumEntered = False
+      else:
+        mutValue = float(mutText)
+    else:
+      if (not (mutText[0:decPtLocation].isdigit() and 
+          mutText[decPtLocation+1:].isdigit())):
+        validNumEntered = False
+      else:
+        decPlace = float(len(mutText[decPtLocation+1:]))
+        divisor = pow(10.0,decPlace)
+        mutValue = float(mutText[0:decPtLocation]) + \
+                  (float(mutText[decPtLocation+1:])/divisor)
+
+    # if they have entered a bad number pull the current value off the slider
+
+    if (not validNumEntered):
+      self.ChangeMutationTextSlot()
+    else:
+      if (mutValue < 0.0):
+        mutValue = 0.0
+      elif (mutValue > 100.0):
+        mutValue = 100.0
+      if mutValue > 0.00001:
+        self.MutationSlider.setValue(int(math.log10(mutValue) * 10000))
+      else:
+        self.MutationSlider.setValue(-30000)
 
   def ChangeWorldSizeTextSlot(self):
     slide_value = str(self.WorldSizeSlider.value())
-    slide_value_txt = slide_value + " x " + slide_value + " cells"
+    slide_value_txt = "x " + slide_value + " cells"
     self.WorldSizeTextLabel.setText(slide_value_txt)
+    self.WorldSizeLineEdit.setText(slide_value)
+
+  # Move the world size slider based on the WorldSizeLineEdit widget
+  # that widget will be sure that a 1 to 3 digit integer is returned
+
+  def ChangeWorldSizeSliderSlot(self):
+    if (not self.WorldSizeLineEdit.text()):
+      tmp_value = self.WorldSizeSlider.value()
+    else:
+      tmp_value = int(str(self.WorldSizeLineEdit.text()))
+    if (tmp_value < self.WorldSizeSlider.minValue()):
+      tmp_value = self.WorldSizeSlider.minValue()
+    elif (tmp_value > self.WorldSizeSlider.maxValue()):
+      tmp_value = self.WorldSizeSlider.maxValue()
+    self.WorldSizeSlider.setValue(tmp_value)
 
   def ChangeStopSpinBoxEnabledSlot(self):
     if self.StopManuallyRadioButton.isChecked() == True:
@@ -202,11 +262,11 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     if settings_dict.has_key("COPY_MUT_PROB") == True:
       copy_mutation_percent = float(settings_dict["COPY_MUT_PROB"]) * 100;
     else:
-      copy_mutation_percent = 0.01
+      copy_mutation_percent = 0.5
     if copy_mutation_percent > 0.00000001:
-      self.MutationSlider.setValue(int(math.log10(copy_mutation_percent) * 100))
+      self.MutationSlider.setValue(int(math.log10(copy_mutation_percent) * 10000))
     else:
-      self.MutationSlider.setValue(-300)
+      self.MutationSlider.setValue(-30000)
     if (settings_dict.has_key("BIRTH_METHOD") and 
         int(settings_dict["BIRTH_METHOD"])) in [0, 1, 2, 3]:
        self.LocalBirthRadioButton.setChecked(True)
@@ -280,7 +340,6 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     # self.StopAtRadioButton.setEnabled(False)
     # self.StopHeadTextLabel.setEnabled(False)
 
-    self.WorldSizeSlider.setEnabled(False)
     self.RandomGeneratedRadioButton.setEnabled(False)
     self.RandomFixedRadioButton.setEnabled(False)
     self.MutationSlider.setEnabled(False)
@@ -304,10 +363,12 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     self.NorCheckBox.setEnabled(False)
     self.XorCheckBox.setEnabled(False)
     self.EquCheckBox.setEnabled(False)
-    self.MutationPercentTextLabel.setEnabled(False)
-    self.WorldSizeTextLabel.setEnabled(False)
+    self.MutationRateLineEdit.setEnabled(False)
     self.MutationRateHeadTextLabel.setEnabled(False)
+    self.WorldSizeTextLabel.setEnabled(False)
     self.WorldSizeHeadTextLable.setEnabled(False)
+    self.WorldSizeSlider.setEnabled(False)
+    self.WorldSizeLineEdit.setEnabled(False)
     self.RewardTextLabel.setEnabled(False)
     self.RandomHeadTextLabel.setEnabled(False)
     self.AncestorHeadTextLabel.setEnabled(False)
@@ -350,10 +411,11 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
     self.NorCheckBox.setEnabled(True)
     self.XorCheckBox.setEnabled(True)
     self.EquCheckBox.setEnabled(True)
-    self.MutationPercentTextLabel.setEnabled(True)
+    self.MutationRateLineEdit.setEnabled(True)
     self.WorldSizeTextLabel.setEnabled(True)
     self.MutationRateHeadTextLabel.setEnabled(True)
     self.WorldSizeHeadTextLable.setEnabled(True)
+    self.WorldSizeLineEdit.setEnabled(True)
     self.RewardTextLabel.setEnabled(True)
     self.RandomHeadTextLabel.setEnabled(True)
     self.AncestorHeadTextLabel.setEnabled(True)
@@ -396,7 +458,7 @@ class pyPetriConfigureCtrl(pyPetriConfigureView):
       settings_dict["RANDOM_SEED"] = 777
     else:
       settings_dict["RANDOM_SEED"] = 0
-    slide_value = float(self.MutationSlider.value())/100.0
+    slide_value = float(self.MutationSlider.value())/10000.0
     slide_value = pow(10,slide_value)
     if slide_value < 0.0011:
       slide_value = 0.0
