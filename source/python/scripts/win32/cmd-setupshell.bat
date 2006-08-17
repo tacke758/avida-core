5d5ec6bcccec210da820a2c8fd273d5f52b98ad9
Kaben Nanlohy <nanlohy@msu.edu>
2006-08-17 17:31:33 +0000

    Added Avida-ED environment setup for dos build.

---
+++ source/python/scripts/win32/cmd-setupshell.bat
@@ -0,0 +1,8 @@
+set BUILD_ROOT=C:\cygwin\home\josh
+set AVIDA_HOME=%BUILD_ROOT%\src\avida-edward\build
+set LOCAL=C:\msys\1.0
+set QTDIR=%BUILD_ROOT%\src\qt-3
+set PATH=%PATH%;%QTDIR%\bin;%QTDIR%\lib;%BUILD_ROOT%\src\gccxml\build\bin;C:\Python24;%AVIDA_HOME%\lib;%AVIDA_HOME%\lib\AvidaCore;C:\MinWG\bin;C:\MinGW\lib;C:\Program Files\NSIS
+
+set PYTHONPATH=C:\Python24\Lib;%AVIDA_HOME%\lib;%BUILD_ROOT%\src\avida-edward\source\python
+
