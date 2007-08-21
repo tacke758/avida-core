f799e121dfe54de89f51efb4b2c8deee6f87189e
Heather Goldsby <hjg@msu.edu>
2007-08-21 01:27:02 +0000

    Enabled scenario satisfaction to include null transitions.

--- source/main/cUMLStateDiagram.cc
+++ source/main/cUMLStateDiagram.cc
@@ -170,7 +170,12 @@ int cUMLStateDiagram::checkForPathStep(std::deque<std::string> path,
 				if (temp > longest_dist) {
 					longest_dist = temp;
 				}	
-			}
+			} else if (ts == "[]/") {
+				temp = checkForPathStep(path, target(ed, sd0), curr_dist);
+				if (temp > longest_dist) { 
+					longest_dist = temp;
+				}
+			}	
 	}
 	
 	//std::cout << "Returning longest_dist " << longest_dist << std::endl;
